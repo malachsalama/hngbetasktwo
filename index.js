82dffcfcff240bfa529de01b9f5a63b2c9cf18f5
Salama <malach.salama@gmail.com>
2023-09-13 17:15:01 +0300

    Error handling

--- index.js
+++ index.js
@@ -33,11 +33,15 @@ const User = mongoose.model("User", userSchema);
 
 // Create a new user
 app.post("/api", async (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ message: "Cannot create new user without details" });
+    return;
+  }
   try {
     const user = new User(req.body);
 
-    const savedUser = await user.save();
-    res.status(201).json(savedUser);
+    await user.save();
+    res.status(201).json({ message: "User created successfully" });
   } catch (err) {
     console.error("Error creating user:", err);
     res.status(500).send("Internal Server Error");
@@ -82,7 +86,7 @@ app.put("/api/:id", async (req, res) => {
     if (!user) {
       res.status(404).send("User not found");
     } else {
-      res.json(user);
+      res.json({ message: "User updated successfully!" });
     }
   } catch (err) {
     console.error("Error updating user:", err);
@@ -98,7 +102,7 @@ app.delete("/api/:id", async (req, res) => {
     if (!user) {
       res.status(404).send("User not found");
     } else {
-      res.json(user);
+      res.json({ message: "User deleted successfully!" });
     }
   } catch (err) {
     console.error("Error deleting user:", err);
